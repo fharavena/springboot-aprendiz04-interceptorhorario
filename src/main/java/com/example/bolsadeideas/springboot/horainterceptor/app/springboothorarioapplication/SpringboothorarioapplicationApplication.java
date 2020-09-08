ff19c9caa052dde4218846f8ad1c4a44407619d5
fharavena <fharavena@gmail.com>
2020-09-08 02:24:27 -0300

    first commit

---
+++ src/main/java/com/example/bolsadeideas/springboot/horainterceptor/app/springboothorarioapplication/SpringboothorarioapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.bolsadeideas.springboot.horainterceptor.app.springboothorarioapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboothorarioapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboothorarioapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bolsadeideas/springboot/horainterceptor/app/springboothorarioapplication/SpringboothorarioapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bolsadeideas.springboot.horainterceptor.app.springboothorarioapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboothorarioapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
